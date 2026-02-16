a2e564c4786b8ae1335e87ccb336ddb5a2e8d8b9
Shel2123 <ac.official.it@gmail.com>
2026-02-16 21:39:06 +0300

    add homework practice 02 notebook for gradient descent implementation

--- hw2/descents.py
+++ hw2/descents.py
@@ -1,23 +1,210 @@
 import numpy as np
-from hw2.interfaces import AbstractOptimizer
+from abc import ABC, abstractmethod
+from interfaces import LearningRateSchedule, AbstractOptimizer, LinearRegressionInterface
 
 
-class VanillaGradientDescent:
-    pass
-class StochasticGradientDescent:
-    pass
-class SAGDescent:
-    pass
-class MomentumDescent:
-    pass
-class Adam:
-    pass
+# ===== Learning Rate Schedules =====
+class ConstantLR(LearningRateSchedule):
+    def __init__(self, lr: float):
+        self.lr = lr
 
+    def get_lr(self, iteration: int) -> float:
+        return self.lr
+
+
+class TimeDecayLR(LearningRateSchedule):
+    def __init__(self, lambda_: float = 1.0):
+        self.s0 = 1
+        self.p = 0.5
+        self.lambda_ = lambda_
+
+    def get_lr(self, iteration: int) -> float:
+        """
+        returns: float, learning rate для iteration шага обучения
+        """
+        lrk = self.lambda_ * ((self.s0 / (self.s0 + iteration)) ** self.p)
+        return lrk
+
+
+# ===== Base Optimizer =====
+class BaseDescent(AbstractOptimizer, ABC):
+    """
+    Оптимизатор, имплементирующий градиентный спуск.
+    Ответственен только за имплементацию общего алгоритма спуска.
+    Все его составные части (learning rate, loss function+regularization) находятся вне зоны ответственности этого класса (см. Single Responsibility Principle).
+    """
+
+    def __init__(self,
+                 lr_schedule: LearningRateSchedule = TimeDecayLR(),
+                 tolerance: float = 1e-6,
+                 max_iter: int = 1000
+                 ):
+        self.lr_schedule = lr_schedule
+        self.tolerance = tolerance
+        self.max_iter = max_iter
+
+        self.iteration = 0
+        self.model: LinearRegressionInterface = None
+
+    @abstractmethod
+    def _update_weights(self) -> np.ndarray:
+        """
+        Вычисляет обновление согласно конкретному алгоритму и обновляет веса модели, перезаписывая её атрибут.
+        Не имеет прямого доступа к вычислению градиента в точке, для подсчета вызывает model.compute_gradients.
+
+        returns: np.ndarray, w_{k+1} - w_k
+        """
+        pass
+
+    def _step(self) -> np.ndarray:
+        """
+        Проводит один полный шаг интеративного алгоритма градиентного спуска
+
+        returns: np.ndarray, w_{k+1} - w_k
+        """
+        delta = self._update_weights()
+        self.iteration += 1
+        return delta
+
+    def optimize(self) -> None:
+        """
+        Оркестрирует весь алгоритм градиентного спуска.
+        """
+        self.model.loss_history.append(self.model.compute_loss())
+        for epoch in range(self.max_iter):
+            d = self._step()
+            loss = self.model.compute_loss()
+            self.model.loss_history.append(loss)
+            if np.isnan(d).any():
+                print("Nan in delta")
+                break
+            if np.sum(np.square(d)) < self.tolerance:
+                break
+
+
+# ===== Specific Optimizers =====
+class VanillaGradientDescent(BaseDescent):
+    def _update_weights(self) -> np.ndarray:
+        X_train = self.model.X_train
+        y_train = self.model.y_train
+        cur_lr = self.lr_schedule.get_lr(self.iteration)
+        cur_grad = self.model.compute_gradients(X_train, y_train)
+        grad_step = -cur_lr * cur_grad
+        self.model.w = self.model.w + grad_step
+        return grad_step
+
+
+class StochasticGradientDescent(BaseDescent):
+    def __init__(self, *args, batch_size=32, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.batch_size = batch_size
+
+    def _update_weights(self) -> np.ndarray:
+        n = self.model.X_train.shape[0]
+        batch_idx = np.random.randint(0, n, size=self.batch_size)
+
+        X_batch = self.model.X_train[batch_idx]
+        y_batch = self.model.y_train[batch_idx]
+        batch_grad = self.model.compute_gradients(X_batch, y_batch)
+
+        cur_lr = self.lr_schedule.get_lr(self.iteration)
+        grad_step = -cur_lr * batch_grad
+
+        self.model.w = self.model.w + grad_step
+        return grad_step
+
+
+class SAGDescent(BaseDescent):
+    def __init__(self, *args, batch_size=32, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.grad_memory = None
+        self.grad_sum = None
+        self.batch_size = batch_size
+
+    def _update_weights(self) -> np.ndarray:
+        X_train = self.model.X_train
+        y_train = self.model.y_train
+        num_objects, num_features = X_train.shape
+
+        if self.grad_memory is None:
+            self.grad_memory = np.zeros((num_objects, num_features), dtype=float)
+
+            for i in range(num_objects):
+                self.grad_memory[i] = self.model.compute_gradients(X_train[i:i + 1], y_train[i:i + 1])
+            self.grad_sum = self.grad_memory.mean(axis=0)
+
+        # batch_idx = np.random.randint(0, num_objects, size=self.batch_size)
+        batch_idx = np.random.choice(num_objects, size=self.batch_size, replace=False)
+        for j in batch_idx:
+            g_old = self.grad_memory[j].copy()
+            g_new = self.model.compute_gradients(X_train[j:j + 1], y_train[j:j + 1])
+
+            self.grad_memory[j] = g_new
+            self.grad_sum += (g_new - g_old) / num_objects
+
+        cur_lr = self.lr_schedule.get_lr(self.iteration)
+        grad_step = -cur_lr * self.grad_sum
+        self.model.w = self.model.w + grad_step
+        return grad_step
+
+
+class MomentumDescent(BaseDescent):
+    def __init__(self, *args, beta=0.9, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.beta = beta
+        self.velocity = None
+
+    def _update_weights(self) -> np.ndarray:
+        if self.velocity is None:
+            self.velocity = np.zeros_like(self.model.w)
+        cur_lr = self.lr_schedule.get_lr(self.iteration)
+        cur_grad = self.model.compute_gradients(self.model.X_train, self.model.y_train)
+
+        self.velocity = self.beta * self.velocity + cur_lr * cur_grad
+        self.model.w = self.model.w - self.velocity
+        return -self.velocity
+
+
+class Adam(BaseDescent):
+    def __init__(self, *args, beta1=0.9, beta2=0.999, eps=1e-8, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.eps = eps
+        self.m = None
+        self.v = None
+
+    def _update_weights(self) -> np.ndarray:
+        X = self.model.X_train
+        y = self.model.y_train
+        if self.m is None or self.v is None:
+            self.m = np.zeros_like(self.model.w)
+            self.v = np.zeros_like(self.model.w)
+
+        cur_grad = self.model.compute_gradients(X, y)
+        cur_lr = self.lr_schedule.get_lr(self.iteration)
+
+        self.m = self.beta1 * self.m + (1 - self.beta1) * cur_grad
+        self.v = self.beta2 * self.v + (1 - self.beta2) * np.square(cur_grad)
+
+        m_hat = self.m / (1 - self.beta1 ** (self.iteration + 1))
+        v_hat = self.v / (1 - self.beta2 ** (self.iteration + 1))
+        grad_step = - cur_lr / (np.sqrt(v_hat) + self.eps) * m_hat
+        self.model.w = self.model.w + grad_step
+        return grad_step
+
+
+# ===== Non-iterative Algorithms ====
 class AnalyticSolutionOptimizer(AbstractOptimizer):
-    def optimize(self, model) -> np.ndarray:
-        X = model.X
-        y = model.y
-        w = np.linalg.inv(X.T @ X) @ (X.T @ y)
-        model.w = w
-        return w
+    """
+    Универсальный дамми-класс для вызова аналитических решений
+    """
+    def optimize(self) -> None:
+        """
+        Определяет аналитическое решение и назначает его весам модели.
+        """
+        # не должна содержать непосредственных формул аналитического решения, за него ответственен другой объект
+        X = self.model.X_train
+        y = self.model.y_train
 
+        self.model.w = self.model.loss_function.analytic_solution(X, y)
--- hw2/linear_regression.py
+++ hw2/linear_regression.py
@@ -1,76 +1,171 @@
-from hw2.descents import AnalyticSolutionOptimizer
-from hw2.interfaces import LossFunction, LinearRegressionInterface
 import numpy as np
-
-class CustomLinearRegression(LinearRegressionInterface):
-    def __init__(self, optimizer: AnalyticSolutionOptimizer, loss_function: LossFunction):
-        self.optimizer = optimizer
-        self.loss_func = loss_function
-
-        self.X_train: np.ndarray | None = None
-        self.y_train: np.ndarray | None = None
-        self.w: np.ndarray | None = None
-
-    @property
-    def X(self) -> np.ndarray:
-        if self.X_train is None:
-            raise RuntimeError("Not fitted: X = None")
-        return self.X_train
-
-    @property
-    def y(self) -> np.ndarray:
-        if self.y_train is None:
-            raise RuntimeError("Not fitted: y = None")
-        return self.y_train
-
-    @staticmethod
-    def _prepare_X(X: np.ndarray) -> np.ndarray:
-        X = np.asarray(X, dtype=float)
-        return X
-
-    @staticmethod
-    def _prepare_y(y: np.ndarray) -> np.ndarray:
-        y = np.asarray(y, dtype=float)
-        return y
-
-    def fit(self, X: np.ndarray, y: np.ndarray):
-        self.X_train = self._prepare_X(X)
-        self.y_train = y
-        self.optimizer.optimize(self)
-        return self
-
-    def predict(self, X: np.ndarray):
-        if self.w is None:
-            raise RuntimeError("Not fitted: w = None")
-        X_ = self._prepare_X(X)
-        return X_ @ self.w
+from interfaces import LossFunction, LossFunctionClosedFormMixin, LinearRegressionInterface, AbstractOptimizer
+from descents import AnalyticSolutionOptimizer
+from typing import Dict, Type, Optional, Callable
+from abc import abstractmethod, ABC
+from scipy.sparse.linalg import svds
+from scipy.sparse import issparse
 
 
-class MSELoss(LossFunction):
-    @staticmethod
-    def _check_batch(X: np.ndarray, y: np.ndarray, indices: np.ndarray | None = None) -> tuple[np.ndarray, np.ndarray]:
-        if indices is not None:
-            return X[indices], y[indices]
-        return X, y
+class MSELoss(LossFunction, LossFunctionClosedFormMixin):
+    def __init__(self, analytic_solution_func: Callable[[np.ndarray, np.ndarray], np.ndarray] = None):
 
+        if analytic_solution_func is None:
+            self.analytic_solution_func = self._plain_analytic_solution
+        else:
+            self.analytic_solution_func = analytic_solution_func
 
-    def loss(self, X: np.ndarray, y: np.ndarray, w: np.ndarray, indices: np.ndarray | None = None) -> float:
-        X, y = self._check_batch(X, y, indices)
+    def loss(self, X: np.ndarray, y: np.ndarray, w: np.ndarray) -> float:
+        """
+        X: np.ndarray, матрица регрессоров
+        y: np.ndarray, вектор таргета
+        w: np.ndarray, вектор весов
+
+        returns: float, значение MSE на данных X,y для весов w
+        """
         n = y.shape[0]
         r = X @ w - y
         Q = (1.0 / n) * (r.T @ r)
-        print(f"y.shape = {y.shape}")
         return float(Q)
 
+    def gradient(self, X: np.ndarray, y: np.ndarray, w: np.ndarray) -> np.ndarray:
+        """
+        X: np.ndarray, матрица регрессоров
+        y: np.ndarray, вектор таргета
+        w: np.ndarray, вектор весов
 
-    def gradient(self, X: np.ndarray, y: np.ndarray, w: np.ndarray, indices: np.ndarray | None = None) -> np.ndarray:
-        X, y = self._check_batch(X, y, indices)
+        returns: np.ndarray, численный градиент MSE в точке w
+        """
         n = y.shape[0]
         grad = (2.0 / n) * (X.T @ (X @ w - y))
-        print(f"y.shape = {y.shape}")
         return grad
 
+    def analytic_solution(self, X: np.ndarray, y: np.ndarray) -> np.ndarray:
+        """
+        Возвращает решение по явной формуле (closed-form solution)
 
-    @staticmethod
-    def _plain_analytic_solution(X: np.ndarray, y: np.ndarray) -> np.ndarray:
+        X: np.ndarray, матрица регрессоров
+        y: np.ndarray, вектор таргета
+
+        returns: np.ndarray, оптимальный по MSE вектор весов, вычисленный при помощи аналитического решения для данных X, y
+        """
+        # Функция-диспатчер в одну из истинных функций для вычисления решения по явной формуле (closed-form)
+        # Необходима в связи c наличием интерфейса analytic_solution у любого лосса;
+        # self-injection даёт возможность выбирать, какое именно closed-form решение использовать
+        return self.analytic_solution_func(X, y)
+
+    @classmethod
+    def _plain_analytic_solution(cls, X: np.ndarray, y: np.ndarray) -> np.ndarray:
+        """
+        X: np.ndarray, матрица регрессоров
+        y: np.ndarray, вектор таргета
+
+        returns: np.ndarray, вектор весов, вычисленный при помощи классического аналитического решения
+        """
         return np.linalg.inv(X.T @ X) @ (X.T @ y)
+
+    @classmethod
+    def _svd_analytic_solution(cls, X: np.ndarray, y: np.ndarray) -> np.ndarray:
+        """
+        X: np.ndarray, матрица регрессоров
+        y: np.ndarray, вектор таргета
+
+        returns: np.ndarray, вектор весов, вычисленный при помощи аналитического решения на SVD
+        """
+        n, m = X.shape
+        kmax = min(n, m) - 1
+
+        U, s, Vt = svds(
+            X, k=kmax, which="LM",
+            solver="arpack",
+            tol=0,
+            maxiter=None,
+        )
+        cut = 1e-8 * max(n, m) * s[0]
+
+        s_inv = np.where(s > cut, 1.0 / s, 0.0)
+        w = Vt.T @ (s_inv * (U.T @ y))
+
+        return w
+
+class L2Regularization(LossFunction):
+    def __init__(self, core_loss: LossFunction, mu_rate: float = 1.0,
+                 analytic_solution_func: Callable[[np.ndarray, np.ndarray], np.ndarray] = None):
+        self.core_loss = core_loss
+        self.mu_rate = mu_rate
+
+        # analytic_solution_func is meant to be passed separately,
+        # as it is not linear to core solution
+
+    def gradient(self, X: np.ndarray, y: np.ndarray, w: np.ndarray) -> np.ndarray:
+        core_part = self.core_loss.gradient(X, y, w)
+
+        penalty_part = ...
+
+        raise NotImplementedError()
+        # TODO: implement
+
+
+class CustomLinearRegression(LinearRegressionInterface):
+    def __init__(
+            self,
+            optimizer: AbstractOptimizer,
+            # l2_coef: float = 0.0,
+            loss_function: LossFunction = MSELoss()
+    ):
+        self.optimizer = optimizer
+        self.optimizer.set_model(self)
+
+        # self.l2_coef = l2_coef
+        self.loss_function = loss_function
+        self.loss_history = []
+        self.w = None
+        self.X_train = None
+        self.y_train = None
+
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        """
+        returns: np.ndarray, вектор \\hat{y}
+        """
+        if self.w is None:
+            raise RuntimeError("Not fitted: w = None")
+        if hasattr(X, "to_numpy") and not issparse(X):
+            X = X.to_numpy()
+        return X @ self.w
+
+    def compute_gradients(self, X_batch: np.ndarray | None = None, y_batch: np.ndarray | None = None) -> np.ndarray:
+        """
+        returns: np.ndarray, градиент функции потерь при текущих весах (self.w)
+        Если переданы аргументы, то градиент вычисляется по ним, иначе - по self.X_train и self.y_train
+        """
+        if X_batch is not None and y_batch is not None:
+            return self.loss_function.gradient(X_batch, y_batch, self.w)
+        return self.loss_function.gradient(self.X_train, self.y_train, self.w)
+
+    def compute_loss(self, X_batch: np.ndarray | None = None, y_batch: np.ndarray | None = None) -> float:
+        """
+        returns: np.ndarray, значение функции потерь при текущих весах (self.w) по self.X_train, self.y_train
+        Если переданы аргументы, то градиент вычисляется по ним, иначе - по self.X_train и self.y_train
+        """
+        if X_batch is not None and y_batch is not None:
+            return self.loss_function.loss(X_batch, y_batch, self.w)
+        return self.loss_function.loss(self.X_train, self.y_train, self.w)
+
+    def fit(self, X: np.ndarray, y: np.ndarray) -> None:
+        """
+        Инициирует обучение модели заданным функцией потерь и оптимизатором способом.
+
+        X: np.ndarray,
+        y: np.ndarray
+        """
+        self.loss_history = []
+        self.optimizer.iteration = 0
+        if hasattr(X, "to_numpy") and not issparse(X):
+            X = X.to_numpy()
+        if hasattr(y, "to_numpy"):
+            y = y.to_numpy()
+        y = np.asarray(y).reshape(-1)
+        self.X_train = X
+        self.y_train = y
+        self.w = np.zeros(X.shape[1], dtype=float)
+        self.optimizer.optimize()
